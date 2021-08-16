e0396dec8606489267c79d166f73caa6272b3f41
Jake Schroeder <jakeschroeder8@gmail.com>
2021-08-16 07:18:00 -0500

    added more ui

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,9 +2,11 @@ import WEBGLAudioVisualizer from './audio-visualizer';
 import { render } from './ui';
 
 let audioVisualizer;
+let isDragging = false;
+let file;
 
 function initApp() {
-    // createAudioVisualer();
+    createAudioVisualer();
     createFileUpload();
     createSearch();
 }
@@ -22,45 +24,35 @@ function createSearch() {
 function createFileUpload() {
     const dragFileToUploadNode = document.getElementById('dragFileToUploadNode');
     const folderImageNode = document.getElementById('folderImageNode') as HTMLImageElement;
-
+    const selectInput = document.getElementById('mp3FileUploadNode');
+    const selectButton = document.getElementById('selectFileBtnNode');
+    selectButton?.addEventListener('click', () => {
+        selectInput?.click();
+    });
     dragFileToUploadNode?.addEventListener('dragover', (event: DragEvent) => {
         event.preventDefault(); //preventing from default behaviour
         dragFileToUploadNode.classList.add('active');
         folderImageNode.src = '/images/folder-open.svg';
     });
     //If user leave dragged File from dragFileToUploadNode
-    dragFileToUploadNode?.addEventListener('dragleave', () => {
+    dragFileToUploadNode?.addEventListener('dragleave', (event: DragEvent) => {
+        event.preventDefault();
         dragFileToUploadNode.classList.remove('active');
+
         folderImageNode.src = '/images/folder-closed.svg';
     });
     //If user drop File on dragFileToUploadNode
     dragFileToUploadNode?.addEventListener('drop', (event: DragEvent) => {
+        dragFileToUploadNode.classList.remove('active');
         event.preventDefault(); //preventing from default behaviour
         //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-        let file = event.dataTransfer?.files[0];
-        console.log(file); //calling function
+        file = event.dataTransfer?.files[0];
         folderImageNode.src = '/images/folder-closed.svg';
     });
-
-    if (dragFileToUploadNode) {
-        //Handle when user drags file over drop zone.
-        dragFileToUploadNode.addEventListener('dragover', (e: DragEvent) => {
-            e.preventDefault();
-            e.dataTransfer?.dropEffect;
-            const target = e.target as HTMLDivElement;
-            target.classList.add('drag-enter');
-        });
-        //Handle when user leaves drop zone
-        dragFileToUploadNode.addEventListener('dragleave', (e: DragEvent) => {
-            e.preventDefault();
-            const target = e.target as HTMLDivElement;
-            target.classList.remove('drag-enter');
-        });
-    }
 }
 
 function createAudioVisualer() {
-    audioVisualizer = new WEBGLAudioVisualizer('container', '/sounds/Marsh feat. Leo Wood - My stripes (Braxton & Marsh Remix).mp3');
+    audioVisualizer = new WEBGLAudioVisualizer('audioVisualizerDisplayNode', '/sounds/Marsh feat. Leo Wood - My stripes (Braxton & Marsh Remix).mp3');
     audioVisualizer.createScene();
     audioVisualizer.animate();
 }
