dcae0db10e925945a9b49a596ce262c93798a20f
Noah Reardon <noah.reardon@vazer.io>
2021-08-17 02:49:59 -0500

    added color gradient and fixed autoleveler

--- src/client/audio-visualizer/index.ts
+++ src/client/audio-visualizer/index.ts
@@ -2,7 +2,7 @@ import { time, timeEnd } from 'console';
 import { normalize } from 'path';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Audio, AudioAnalyser, Clock, PerspectiveCamera, Points, PointsMaterial, Scene, WebGLRenderer } from 'three';
+import { Audio, AudioAnalyser, Clock, Color, PerspectiveCamera, Points, PointsMaterial, Scene, WebGLRenderer } from 'three';
 import { clamp } from 'three/src/math/MathUtils';
 import { totalmem } from 'os';
 
@@ -11,6 +11,7 @@ const myBtn = document.getElementById("playButton");
 
 
 type Div = HTMLDivElement | null;
+
 class WEBGLAudioVisualizer {
     private containerDOMNode: Div;
     private audioSource: string;
@@ -28,6 +29,7 @@ class WEBGLAudioVisualizer {
     private points: Points;
 
     private positions: number[];
+    private colors: number[];
     private startTime: number;
     private analyzer: THREE.AudioAnalyser;
     private soundData: Uint8Array;
@@ -44,7 +46,7 @@ class WEBGLAudioVisualizer {
     private floatingMaxZHeight = 0;
     private totalAmp = 0;
     private maxRadius = 2000;
-    private targetZHeight = 200;
+    private targetZHeight = 300;
 
     private sideLength: number;
 
@@ -61,9 +63,28 @@ class WEBGLAudioVisualizer {
     private totalCurrentAvgZHeight: number;
     private zHeightSum: number[];
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     constructor(containerDOMNodeId: string, audioSource: string) {
 
-        
         this.audioSource = audioSource;
         this.listener = new THREE.AudioListener();
         this.sound = new THREE.Audio(this.listener);
@@ -72,10 +93,8 @@ class WEBGLAudioVisualizer {
             this.loadAudio();
         })
 
-
         this.containerDOMNode = document.getElementById(containerDOMNodeId) as Div;
         
-
         this.startTime = Date.now();
         this.renderer = new THREE.WebGLRenderer();
         this.camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 5, 10000);
@@ -89,8 +108,9 @@ class WEBGLAudioVisualizer {
         this.material = new THREE.PointsMaterial({ size: 15, vertexColors: true });
         this.points = new THREE.Points(this.geometry, this.material);
 
-        this.freqChannels = 32;
+        this.freqChannels = 64;
         this.positions = [];
+        this.colors = [];
         this.soundData = new Uint8Array();
         this.previousAvgAmp = [];
         this.analyzer = new THREE.AudioAnalyser(this.sound, this.freqChannels * 2);
@@ -101,7 +121,7 @@ class WEBGLAudioVisualizer {
         this.orbitControls.rotateSpeed = 0.1;
 
         this.particles = 100000;
-        this.particleSpacing = 16;
+        this.particleSpacing = 20;
         this.sideLength = Math.sqrt(this.particles);
 
         this.integral = 0;
@@ -111,13 +131,44 @@ class WEBGLAudioVisualizer {
         this.zHeightSum = [];
     }
 
+
+
+
+
+
+
+
+
+
+    private scale (number: number, inMin: number, inMax: number, outMin: number, outMax: number) {
+        let scaledValue = ( (number - inMin) / (inMax - inMin) ) * (outMax - outMin) + outMin
+        scaledValue = clamp(scaledValue , outMin, outMax);
+        if( scaledValue ){
+            return scaledValue
+        }else{
+            return 0
+        }
+    }
+
+    private onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+
+
+
+
+
+
     public createScene() {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setPixelRatio(window.devicePixelRatio);
         if (this.containerDOMNode) this.containerDOMNode.appendChild(this.renderer.domElement);
         window.addEventListener('resize', this.onWindowResize.bind(this));
 
-        //this.loadAudio();
+        this.loadAudio();
         this.createParticles();
     }
 
@@ -135,7 +186,7 @@ class WEBGLAudioVisualizer {
     }
 
     private createParticles() {
-        const colors: number[] = [];
+        
         let color = new THREE.Color();
 
         for (let i = 0; i < this.particles; i++) {
@@ -147,8 +198,8 @@ class WEBGLAudioVisualizer {
                 this.particlesRemainingInCircle = this.particlesInCircle;
             }
             this.particlesRemainingInCircle --;
-
             let theta = 2 * Math.PI * (this.particlesRemainingInCircle / this.particlesInCircle);
+
             let x = this.radius * Math.sin(theta)
             let y = this.radius * Math.cos(theta)
             let z = 0;
@@ -158,37 +209,106 @@ class WEBGLAudioVisualizer {
             color.r = 1 - clamp( this.radius / this.maxRadius - 0.3 , 0, 1);
             color.g = 0
             color.b = 0
-            //color.setColorName('red');
-            colors.push(color.r, color.g, color.b);
+            this.colors.push(color.r, color.g, color.b);
         }
 
         this.geometry.setAttribute('position', new THREE.Float32BufferAttribute(this.positions, 3));
-        this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+        this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(this.colors, 3));
         this.geometry.computeBoundingSphere();
 
         this.points.geometry.attributes.position.needsUpdate = true;
         this.scene.add(this.points);
     }
 
-    private scale (number: number, inMin: number, inMax: number, outMin: number, outMax: number) {
-        let scaledValue = ( (number - inMin) / (inMax - inMin) ) * (outMax - outMin) + outMin
-        scaledValue = clamp(scaledValue , outMin, outMax);
-        if( scaledValue ){
-            return scaledValue
-        }else{
-            return 0
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public animate(): void {
+        requestAnimationFrame(this.animate.bind(this));
+        this.soundData = this.analyzer.getFrequencyData();
+        this.showParticles();
+        this.render();
     }
+
+    private render() {
+        const time = -40 + (Date.now() - this.startTime) * 0.001;
+
+        let oscillationTime = 10;
+        let sweepAngle = 1;
+
+        this.points.rotation.y = Math.sin(time / oscillationTime) * sweepAngle;
+        this.points.rotation.z = Math.sin(time / oscillationTime) * sweepAngle;
+        this.points.rotation.x = -Math.sin(5 + time / oscillationTime) * sweepAngle;
+
+        this.renderer.render(this.scene, this.camera);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // per frame
+
+    // delta time update
+    // update EWMA average
+    // update normalized freq amp list
+    // update normalized total frame amp
+    // update total average z height of non-zero particles z height 
+    // update total current max height
+
+
+
+
+    // for each particle
+
+    // update x,y
+    // update normalized dist from center
+    // scale based on:
+    //      Beat : current frame amp vs EWMA average
+    //      Volume : current frame amp scales all z heights
+    //      Speaker Shape : adjust scaling to create speaker disk shape (particles far from center move more - create cone shape)
+    //      intertia : 
+    // interpolate : smooth between freq amps
+    // equalize : re-maps z value to larger range
+
+
+
+    // create center cap
+    //
+
+
+
+
 
     private showParticles() {
         const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
         
         let deltaTime = Date.now() - this.previousTime;
         this.previousTime = Date.now();
-        let centerX = 0;
-        let centerY = 0;
 
-        //console.log(this.maxRadius);
+        //console.log(this.floatingMaxZHeight);
 
         let amp: number[] = [this.freqChannels];
         this.totalAmp = 0;
@@ -198,7 +318,7 @@ class WEBGLAudioVisualizer {
         let totalCurrentMaxHeight = 0;
         this.totalAvgVolume = 0;
         let EWMA_average = 0;
-        let EWMA_range = 30;
+        let EWMA_range = 10;
 
         for (let n = 0; n < Math.max(this.previousAvgAmp.length, this.freqChannels); n++) {
             //averages the last 10 frames of avg volume data (normalized gaussian distribution)
@@ -234,7 +354,7 @@ class WEBGLAudioVisualizer {
             let x = this.positions[i];
             let y = this.positions[i + 1];
 
-            let distFromCenter = Math.sqrt(Math.abs(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2)));
+            let distFromCenter = Math.sqrt(Math.abs(Math.pow(x, 2) + Math.pow(y, 2)));
 
             let z = 0;
 
@@ -260,8 +380,9 @@ class WEBGLAudioVisualizer {
                     z -= z * (normalizedDistFromCenter * this.freqChannels - pointFrequencyAssignemnt);
                 }
 
-                z += z * this.maxAmp * this.totalAmp
-                z += z * 0.1 * this.beat_multiplier
+                z *= z * z
+                z += z * 0.5 * this.maxAmp * this.totalAmp
+                z += z * 0.3 * this.beat_multiplier
 
                 //z += (z * z) * this.totalCurrentAvgZHeight
             } else {
@@ -269,14 +390,14 @@ class WEBGLAudioVisualizer {
             }
 
             //radial position based height rebalancer (creates the dome shap for the vitual speaker)
-            z *= 10  * ( distFromCenter / this.maxRadius)
+            z *= 5  * ( distFromCenter / this.maxRadius)
 
             //z *= 100 * integralCorrectionFactor
 
             //z = clamp(z,-3000,3000)
 
-            z = this.scale(z,0,500,0, this.equalizer)
-
+            z = this.scale(z,0,1000,0, this.equalizer)
+            
 
             if(distFromCenter < 300 && z != 0){
                 z -= z*(80 - 80 * Math.pow(distFromCenter/300, 2) ) /80
@@ -290,18 +411,20 @@ class WEBGLAudioVisualizer {
 
             totalCurrentMaxHeight = Math.max(totalCurrentMaxHeight, z)
 
-            if(totalCurrentMaxHeight > this.floatingMaxZHeight)
-            {
-                this.floatingMaxZHeight += 1
-            }else{
-                this.floatingMaxZHeight -= 1
-            }
-    
+            this.colors[i] = z / this.targetZHeight
+            
             //set Z pos and clamp max / min height
             this.positions[i + 2] = z;
             
         }
 
+
+        if(totalCurrentMaxHeight > this.floatingMaxZHeight)
+        {
+            this.floatingMaxZHeight += 5
+        }else{
+            this.floatingMaxZHeight -= 5
+        }
 
         if(this.zHeightSum.length != 0){
             this.totalCurrentAvgZHeight = averageZHeightRunningSum / this.zHeightSum.length
@@ -310,11 +433,11 @@ class WEBGLAudioVisualizer {
 
         if(this.floatingMaxZHeight != 0){
             if( totalCurrentMaxHeight <= this.targetZHeight ){
-                this.maxRadius +=  0.1 * Math.abs(this.targetZHeight - this.floatingMaxZHeight) //* (this.targetZHeight - this.floatingMaxZHeight)
-                this.equalizer += 1 * Math.abs(this.targetZHeight - this.floatingMaxZHeight) //* (this.targetZHeight - this.floatingMaxZHeight) //(1.2 * this.floatingMaxZHeight) * (deltaTime / 1000)
+                this.maxRadius +=  0.15 * Math.abs(this.targetZHeight - this.floatingMaxZHeight) //* (this.targetZHeight - this.floatingMaxZHeight)
+                this.equalizer += 5 * Math.abs(this.targetZHeight - this.floatingMaxZHeight) //* (this.targetZHeight - this.floatingMaxZHeight) //(1.2 * this.floatingMaxZHeight) * (deltaTime / 1000)
             }else{
-                this.maxRadius -= 0.1 * Math.abs(this.floatingMaxZHeight - this.targetZHeight)  //* (this.targetZHeight - this.floatingMaxZHeight)
-                this.equalizer -= 1 * Math.abs(this.floatingMaxZHeight - this.targetZHeight) //* (this.targetZHeight - this.floatingMaxZHeight) //(0.1 * this.floatingMaxZHeight) 
+                this.maxRadius -= 0.15 * Math.abs(this.floatingMaxZHeight - this.targetZHeight)  //* (this.targetZHeight - this.floatingMaxZHeight)
+                this.equalizer -= 5 * Math.abs(this.floatingMaxZHeight - this.targetZHeight) //* (this.targetZHeight - this.floatingMaxZHeight) //(0.1 * this.floatingMaxZHeight) 
             }
         }
 
@@ -330,40 +453,30 @@ class WEBGLAudioVisualizer {
         //console.log(error)
 
         this.geometry.setAttribute('position', new THREE.Float32BufferAttribute(this.positions, 3));
+        this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(this.colors, 3));
+
+        //this.camera.position.z = this.scale(this.floatingMaxZHeight * 2,0,500,2000,6000)
+
+
     }
 
-    public animate(): void {
-        requestAnimationFrame(this.animate.bind(this));
-        this.soundData = this.analyzer.getFrequencyData();
-        this.showParticles();
-        this.render();
-    }
 
-    private render() {
-        const time = -40 + (Date.now() - this.startTime) * 0.001;
 
-        let oscillationTime = 10;
-        let sweepAngle = 1;
 
-        this.points.rotation.y = Math.sin(time / oscillationTime) * sweepAngle; //Math.sin(time / 20) * 5;
-        this.points.rotation.z = Math.sin(time / oscillationTime) * sweepAngle;
-        this.points.rotation.x = -Math.sin(5 + time / oscillationTime) * sweepAngle;
-        // this.camera.position.z = Math.sin((2 * time + 50 * this.userZoomLevel) / oscillationTime) * 1000 + 2600;
 
-        //this.points.position.y = Math.sin(time / oscillationTime) * sweepAngle; //Math.sin(time / 20) * 5;
-        //this.points.position.z = Math.sin(time / oscillationTime) * sweepAngle;
-        //this.points.position.x = -Math.sin(5 + time / oscillationTime) * sweepAngle;
 
-        this.userZoomLevel = 2 * Math.sin(time);
 
-        this.renderer.render(this.scene, this.camera);
-    }
 
-    private onWindowResize() {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+
+
+
+
+
+
+
+
+
+
 }
 
 export default WEBGLAudioVisualizer;
